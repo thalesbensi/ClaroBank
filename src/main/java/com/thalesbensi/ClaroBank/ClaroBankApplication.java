3f219ba12ebe851807a2b4d21857ebec8c6288fd
thalesbensi <thalesbensi@outlook.com>
2025-02-20 12:28:22 -0300

    Project Setup

---
+++ src/main/java/com/thalesbensi/ClaroBank/ClaroBankApplication.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.ClaroBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroBankApplication.class, args);
+	}
+
+}
